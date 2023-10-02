d20561828609bbb54fb80345e193c85ebddfbc70
Deepak Raj <99645724+DeepakRajjj@users.noreply.github.com>
2023-10-03 01:58:49 +0530

    Create operators_2october.js

---
+++ operators_2october.js
@@ -0,0 +1,123 @@
+// // ------- Write a function that takes two numbers as input and returns their sum, difference, product, quotient, and remainder.
+
+// function calculator(value1,value2)
+// {
+//     return("Addition--"+(value1+value2)+"  Subtraction--"+(value1-value2)+"  Multiplication--"+(value1*value2)+"  Division--"+(value1%value2));
+// }
+// var value1=5;
+// var value2=75;
+// console.log(calculator(value1,value2));
+
+
+
+// //------- Write a function that takes a number as input and returns its absolute value.
+// function Absolute_value(value1)
+// {
+//     return(Math.abs(value1));
+// }
+// var value1=5.999;
+// console.log(Absolute_value(value1));
+
+
+
+// // -------- Write a function that takes a number as input and returns its square root.
+// function square_root_value(value1)
+// {
+//     return(Math.sqrt(value1));
+// }
+// var value1=16;
+// console.log(square_root_value(value1));
+
+
+// // ------- Write a function that takes two numbers as input and returns the larger of the two.
+// function larger_value(value1,value2)
+// {
+//     if(value1>value2)
+//     {
+//         return("value1 "+value1+" is greater");
+//     }
+//     else if(value2>value1){
+//         return("value2 "+value2+" is greater");
+//     }
+//     else{
+//         return("both are equal");
+//     }
+// }
+// var value1=16;
+// var value2=5;
+// console.log(larger_value(value1,value2));
+
+
+
+// // ------ Write a function that takes a number as input and returns a new number that is one greater than the original number.
+// function increement_value(value1)
+// {
+//     return(++value1);``
+// }
+// var value1=16;
+// console.log(increement_value(value1));
+
+
+
+// // ------ Write a function that takes two strings as input and returns a new string that is the concatenation of the two original strings.
+// function concaty(value1,value2)
+// {
+//     return(value1+value2);
+// }
+// var value1="Hello";
+// var value2="Deepu";
+// console.log(concaty(value1,value2));
+
+
+// // -----Write a function that takes two numbers as input and returns true if the first number is greater than the second number, and false otherwise.
+// function larger_value(value1,value2)
+// {
+//     if(value1>value2)
+//     {
+//         return true;
+//     }
+//     else if(value2>value1){
+//         return false;
+//     }
+//     else{
+//         return("both are equal");
+//     }
+// }
+// var value1=16;
+// var value2=5;
+// console.log(larger_value(value1,value2));
+
+
+// // ------- Write a function that takes a string as input and returns true if the string is empty, and false otherwise.
+// function Is_empty_string(value1)
+// {
+//     if(value1==="")
+//     {
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// var value1="";
+// console.log(Is_empty_string(value1));
+
+
+
+// //------Write a function that takes an object as input and returns true if the object is empty, and false otherwise.
+// function Is_empty_object(value1)
+// {
+//     return Object.keys(value1).length === 0;
+// }
+// const value1={name:"Deepu"};
+// console.log(Is_empty_object(value1));
+
+
+
+// //-----Write a function that takes an object as input and returns a new object that is a copy of the original object.
+// function copy_object(new_obj)
+// {
+//     return new_obj;
+// }
+// const value1={name:"Deepu",roll:8};
+// console.log(copy_object(value1));
