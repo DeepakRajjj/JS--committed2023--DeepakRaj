658ce17a9834804f17c2cabc67c9b5bda9b496e9
Deepak Raj <99645724+DeepakRajjj@users.noreply.github.com>
2023-10-07 12:53:57 +0530

    Create if_else.js

---
+++ if_else.js
@@ -0,0 +1,52 @@
+// // -------Write a JavaScript function that takes a day of the week as input and returns the corresponding activity:
+// //  "Monday" for "work", "Tuesday" for "gym", "Wednesday" for "shopping", "Thursday" for "movies", "Friday" for "party",
+// //  "Saturday" for "relax", and "Sunday" for "sleep".
+
+// function routine(day){
+//     if(day==="Monday")
+//     {
+//         return "Monday for work";
+//     }
+//     else if(day==="Tuesday")
+//     {
+//         return "Tuesday for gym";
+//     }
+//     else if(day==="Wednesday")
+//     {
+//         return "Wednesday for shopping";
+//     }
+//     else if(day==="Thursday")
+//     {
+//         return "Thursday for movies";
+//     }
+//     else if(day==="Friday")
+//     {
+//         return "Friday for party";
+//     }
+//     else if(day==="Saturday")
+//     {
+//         return "Saturday for relax";
+//     }
+//     else if(day==="Sunday")
+//     {
+//         return "Sunday for sleep";
+//     }
+//     else {"Wrong input"};
+// }
+// let work = routine("Monday");
+// console.log(work)
+
+
+
+// // ------ Write a program to check if a number is even or odd.
+// function checker(input)
+// {
+//   if(input%2==0)
+//   {
+//     return "even";
+//   }
+//   else
+//   return "odd";
+// }
+// let value = 3;
+// console.log(checker(value));
